fe6a506249ccf1d3c038b510486ab432d746c712
Gabriella Alves <oliveira.gabriellaalves@gmail.com>
2024-06-16 18:40:18 -0300

    16-06-2024

--- src/pages/Estoque/Estoque.js
+++ src/pages/Estoque/Estoque.js
@@ -1,33 +1,67 @@
-import React, { useContext } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useEffect, useState, useContext } from 'react';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+// import Paper from '@mui/material/Paper';
 import './estoqueStyle.css';
-import { SidebarContext } from '../../components/sidebar/SidebarContext.js'; // Ajuste o caminho conforme necessário
+import { SidebarContext } from '../../components/sidebar/SidebarContext.js';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
 
 export default function Estoque() {
   const { isSidebarActive } = useContext(SidebarContext);
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [error, setError] = useState(null);
+  
+  const fetchProdutos = async () => {
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch('http://localhost:8080/estoque', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      });
 
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+
+        // Pegando as chaves do primeiro objeto como nomes das colunas
+        if (data.length > 0) {
+          setColumns(Object.keys(data[0]));
+        }
+      } else {
+        console.error('Erro ao buscar os dados.');
+        setError('Erro ao buscar os dados.');
+      }
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+      setError('Erro na requisição');
+    }
+  };
+  useEffect(() => {
+    fetchProdutos();
+  }, []);
   return (
     <div className={`table ${isSidebarActive ? 'with-sidebar' : ''}`}>
         <h2>Estoque</h2>
-        <TableContainer component={Paper}>
+        {/* <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
             <TableRow>
@@ -50,7 +84,32 @@ export default function Estoque() {
             ))}
             </TableBody>
         </Table>
-        </TableContainer>
+        </TableContainer> */}
+        <div>
+          {error && <p>{error}</p>}
+          {data.length > 0 ? (
+            <table>
+              <thead>
+                <tr>
+                  {columns.map((column, index) => (
+                    <th key={index}>{column}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    {columns.map((column, colIndex) => (
+                      <td key={colIndex}>{item[column]}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>Nenhum dado disponível</p>
+          )}
+        </div>
     </div>
   );
 };
