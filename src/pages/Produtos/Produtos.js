7ad556f95498ea6b2b59a28ef0a72dec1473d7de
Gabriella Alves <oliveira.gabriellaalves@gmail.com>
2024-06-17 18:52:58 -0300

    17-06-2024

--- src/pages/Produtos/Produtos.js
+++ src/pages/Produtos/Produtos.js
@@ -35,9 +35,19 @@ export default function Produtos() {
       setError('Erro na requisição');
     }
   };
+
   useEffect(() => {
     fetchProdutos();
   }, []);
+
+  const updateProduct = () => {
+    console.log("Hello World!");
+  }
+
+  const deleteProduct = () => {
+    console.log("Hello World!");
+  }
+
   return (
     <div id='div-produtos' className={`table ${isSidebarActive ? 'with-sidebar' : ''}`}>
         <h2>Produtos</h2>
@@ -58,14 +68,14 @@ export default function Produtos() {
                 {data.map((item, index) => (
                   <tr key={index}>
                     <td>
-                      <button>
+                      <button onClick={deleteProduct()}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                           <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/>
                         </svg>
                       </button>
                     </td>
                     <td>
-                      <button>
+                      <button onClick={updateProduct()}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                           <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/>
                         </svg>
