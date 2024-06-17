85f5c0d3bb32da7788b0a549bdf2a6187b64e82b
Gabriella Alves <oliveira.gabriellaalves@gmail.com>
2024-06-17 17:02:30 -0300

    17-06-2024

--- src/pages/Produtos/Produtos.js
+++ src/pages/Produtos/Produtos.js
@@ -33,7 +33,6 @@ export default function Produtos() {
       const response = await fetch('http://localhost:8080/produto', {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json',
           'Authorization': `${token}`,
         },
       });
