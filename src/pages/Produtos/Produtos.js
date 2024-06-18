c3ac2a2e25e5fb346ee44c16daa954273b956e09
Gabriella Alves <oliveira.gabriellaalves@gmail.com>
2024-06-17 23:01:34 -0300

    17-06-2024

--- src/pages/Produtos/Produtos.js
+++ src/pages/Produtos/Produtos.js
@@ -122,14 +122,19 @@ export default function Produtos() {
     }
   };
 
-  const deleteProduct = async (productName) => {
+  const deleteProduct = async (productName, stockId) => {
     const token = localStorage.getItem('token');
+    const payload = {
+      estoqueId: stockId,
+    };
+
     try {
       const response = await fetch(`http://localhost:8080/produto/${productName}`, {
         method: 'DELETE',
         headers: {
           'Authorization': `${token}`,
         },
+        body: JSON.stringify(payload),
       });
 
       if (response.ok) {
@@ -258,7 +263,7 @@ export default function Produtos() {
                       </button>
                     </td>
                     <td>
-                      <button onClick={() => deleteProduct(item.nome)}>
+                      <button onClick={() => deleteProduct(item.nome, item.estoqueId)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                           <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/>
                         </svg>
