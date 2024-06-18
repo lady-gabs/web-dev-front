3a668557b97e8058518e2c94513a043836e0fbb1
Gabriella Alves <oliveira.gabriellaalves@gmail.com>
2024-06-17 23:35:27 -0300

    17-06-2024

--- src/pages/Produtos/Produtos.js
+++ src/pages/Produtos/Produtos.js
@@ -121,12 +121,12 @@ export default function Produtos() {
     }
   };
 
-  const deleteProduct = async (productName, estoqueId) => {
+  const deleteProduct = async (item) => {
     const token = localStorage.getItem('token');
-    const payload = { estoqueId: estoqueId };
-
+    const payload = { estoqueId: item.estoqueId };
+    
     try {
-      const response = await fetch(`http://localhost:8080/produto/${productName}`, {
+      const response = await fetch(`http://localhost:8080/produto/${item.nome}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
@@ -136,7 +136,7 @@ export default function Produtos() {
       });
 
       if (response.ok) {
-        setData(prevData => prevData.filter(product => product.nome !== productName));
+        setData(prevData => prevData.filter(product => product.nome !== item.nome));
       } else {
         console.error('Erro ao deletar o produto.');
         setError('Erro ao deletar o produto.');
@@ -261,7 +261,7 @@ export default function Produtos() {
                       </button>
                     </td>
                     <td>
-                      <button onClick={() => deleteProduct(item.nome, item.estoqueId)}>
+                      <button onClick={() => deleteProduct(item)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                           <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/>
                         </svg>
